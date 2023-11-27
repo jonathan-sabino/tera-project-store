2b9afa929a44361d92f0a152d4b9a73c9dff4689
Jonathan Sabino <jsabinomiranda@gmail.com>
2023-11-27 18:52:16 -0300

    Altera register button 100%

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -49,7 +49,7 @@ const Agreement = styled.div`
 const Checkbox = styled.input.attrs({ type: "checkbox" })``;
 
 const Button = styled.button`
-  width: 40%;
+  width: 100%;
   border: none;
   padding: 15px 20px;
   background-color: teal;
